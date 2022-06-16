18f80422939d3f59c5ccb9e7b2ca35fd5a386b36
testkudosta <107615088+testkudosta@users.noreply.github.com>
2022-06-16 18:08:58 +0530

    okay

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php 
-<<<<<<< HEAD
-echo "test git setup start New abc";
-=======
-echo "test git setup okay 111";
->>>>>>> 5d21b1d9dac6b1292d71868372b29255266e2df4
+    echo "test git setup okay Testing";
 ?>
