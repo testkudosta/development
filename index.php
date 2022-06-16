ad97e862b24d13935b8a161c1283d07daadd4999
testkudosta <107615088+testkudosta@users.noreply.github.com>
2022-06-16 18:07:50 +0530

    init

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php 
-echo "test git setup okay";
+<<<<<<< HEAD
+echo "test git setup start New abc";
+=======
+echo "test git setup okay 111";
+>>>>>>> 5d21b1d9dac6b1292d71868372b29255266e2df4
 ?>
